26050e1b7b7faaecd4283aa11bfbb404a3335269
Sean Adhikari <sean.adhikari@glueup.com>
2022-02-17 18:03:29 -0500

    Initial Commit

---
+++ FlikrImages/FlikrImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlikrImagesApp.swift
+//  FlikrImages
+//
+//  Created by Sean Adhikari on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FlikrImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
